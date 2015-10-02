03baad12f0991f60b68ee453280ef794ff67f4c2
mie00 <mielawadi@gmail.com>
2015-10-02 09:16:43 +0200

    added book models

---
+++ src/models/book.js
@@ -0,0 +1,90 @@
+var dbFactory = require('odbc'),
+    util = require('util');
+
+var arToUTF = require('../utils/encoding').arToUTF,
+    config = require('../../config');
+
+var cb = require('../../src/utils/functions').cb;
+var map = require('../../src/utils/objects').map;
+
+var Book = module.exports = function(id, options, callback) {
+
+    options.dir = (options.dir) ? util.format('%s/%d.mdb', options.dir, id) : util.format('%s/Books/%d/%d.mdb', config.dir, id % 10, id);
+    var self = {};
+    var db = dbFactory();
+    self.open = function(callback) {
+        var cn = util.format('DRIVER={MDB};CHARSET=CP1256;DBQ=%s', options.dir);
+        db.open(cn, function(err) {
+            if (err) callback(err);
+            else {
+                self.is_opened = true;
+                callback(null, self)
+            }
+        });
+    };
+    self.close = function(callback) {
+        db.close(function(err) {
+            if (!err)
+                self.is_opened = false;
+            callback(err);
+        });
+    };
+    self.is_opened = false;
+    self.titles = function(callback) {
+        db.query("SELECT * FROM title\0", cb(function(res) {
+            return res.map(function(res) {
+                res.tit = arToUTF(res.tit)
+                return res;
+            })
+        }, callback));
+    };
+    self.ids = function(callback) {
+        db.query("SELECT id FROM book\0", cb(function(res) {
+            return res.map(function(res) {
+                return res.id
+            })
+        }, callback));
+    };
+    self.flId = function(callback) {
+        self.ids(cb(function(res) {
+            return {
+                first: Math.min.apply(Math, res),
+                last: Math.max.apply(Math, res)
+            };
+        }, callback));
+    };
+    self.fId = function(callback) {
+        self.flId(cb(function(res) {
+            return res.first;
+        }, callback));
+    };
+    self.lId = function(callback) {
+        self.flId(cb(function(res) {
+            return res.last;
+        }, callback));
+    };
+    self.getStartId = function(obj, callback) {
+        var criteria = map(obj, function(x, y) {
+            return util.format('%s = %s', x, y)
+        }).join(' AND ');
+        db.query(util.format("SELECT id FROM book where %s \0", criteria), cb(function(res) {
+            return Math.min.apply(Math, res.map(function(res) {
+                return res.id
+            }))
+        }, callback));
+    };
+    self.getWithId = function(id, callback) {
+        db.query(util.format("SELECT * FROM book where id=%d \0", id), cb(function(res) {
+            return res;
+        }, callback));
+    }
+    self.goto = function(criteriaObj, callback) {
+        self.getStartId(criteriaObj, function(err,id) {
+            self.getWithId(id, cb(function(x) {
+                return x;
+            }, callback))
+        })
+    }
+    self.open(callback);
+    return self
+};
---
+++ test/models/book.js
@@ -0,0 +1,130 @@
+var should = require("should"),
+    sinon = require('sinon');
+
+var isNotAr = require('../../src/utils/encoding').isNotAr,
+    Book = require('../../src/models/book.js');
+
+var config = require('../../config');
+config.dir = './Shamela-mock'
+
+describe('Book model', function() {
+    describe('constructor', function() {
+        it('should return the book object', function(done) {
+            var book = Book(4685, {}, function(err, res) {
+                done(res != book)
+            });
+        });
+        it('should execute the callback', function(done) {
+            var book = Book(4685, {}, function(err, res) {
+                done();
+                res.close(function() {});
+            });
+        });
+        it('should accept custom book dirs', function(done) {
+            Book(3685, {
+                dir: './'
+            }, function(err, res) {
+                done(err);
+            })
+        });
+    });
+
+    describe('instance', function() {
+        var book;
+        before(function(done) {
+            Book(170, {}, function(err, res) {
+                book = res;
+                done(err);
+            })
+        })
+        it('should get all titles with ids', function(done) {
+            book.titles(function(err, res) {
+                res.should.be.an.Array();
+                done(err)
+            })
+        });
+        it('should get titles encoded in utf-8', function(done) {
+            book.titles(function(err, res) {
+                res[0].tit.should.be.a.String();
+                isNotAr(res[0].tit).should.be.true()
+                done(err)
+            })
+        });
+        it('should get ids', function(done) {
+            book.ids(function(err, ids) {
+                ids.should.containDeep([4, 10, 1003])
+                ids.should.not.containDeep([3])
+                done(err);
+            })
+        });
+        it('should get first,last id', function(done) {
+            book.flId(function(err, ids) {
+                ids.first.should.equal(4);
+                ids.last.should.equal(7110);
+                done(err);
+            })
+        });
+        it('should get first id', function(done) {
+            book.fId(function(err, first) {
+                first.should.equal(4);
+                done(err);
+            })
+        });
+        it('should get last id', function(done) {
+            book.lId(function(err, last) {
+                last.should.equal(7110);
+                done(err);
+            })
+        });
+        it('should move get start id of a give criteria', function(done) {
+            book.getStartId({
+                part: 1,
+                page: 10
+            }, function(err, res) {
+                done(res != 49);
+            })
+        });
+        it('should get a specific id', function(done) {
+            book.getWithId(49, function(err, res) {
+                res.should.not.be.empty();
+                res.should.matchEach(function(res) {
+                    res.should.be.an.Object();
+                    res.should.have.ownProperty('id');
+                    res.id.should.be.a.Number();
+                    res.id.should.equal(49);
+                    res.should.have.ownProperty('nass');
+                    res.nass.should.be.a.String();
+                });
+                done(err)
+            })
+        });
+        it('should get page with a specific criteria', function(done) {
+            this.slow()
+            book.goto({
+                part: 1,
+                page: 10
+            }, function(err, res) {
+                console.log(res)
+                res.should.not.be.empty();
+                res.should.matchEach(function(res) {
+                    res.should.be.an.Object();
+                    res.should.have.ownProperty('id');
+                    res.id.should.equal(49);
+                });
+                done(err)
+            })
+        });
+        it('should be closable', function(done) {
+            var book = Book(4685, {}, function(err, res) {
+                res.close(function() {
+                    book.is_opened.should.be.false();
+                    should(err).be.null();
+                    res.titles(function(err) {
+                        should(err).not.be.null();
+                        done()
+                    });
+                });
+            });
+        });
+    });
+});
